e8b7c88fa7963adfa069b7fc5abdc7c4dd383b4e
PedroLopes2006 <pedro_magnago_lopes@hotmail.com>
2025-09-09 22:18:45 -0300

    Add files via upload

--- vetores/main.c
+++ vetores/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAMANHO 50
+#define TAMANHO 3
 
 
 int main()
 {
     int matricula[TAMANHO];
-    float bimestre[TAMANHO], soma, media;
-    int indice = 0;
-    int opcao, i;
+    float bimestre[TAMANHO], soma, media, aux2;
+    int indice = 0; //START
+    int opcao, i, output, matPesquisa;
+    int Ls, aux1, Flag;
 
     printf("MENU DE OPCOES:\n");
     printf("[1] Cadastrar o aluno\n");
@@ -78,11 +79,90 @@ int main()
         }
         else if(opcao == 3)
         {
-            printf("Relatorio 2\n");
+            if(indice == 0)
+            {
+                printf("Pauta Vazia.\n");
+            }
+            else
+            {
+               printf("Escolha a Matricula (XXX): ");
+               scanf("%i", &matPesquisa);
+               //TRATAMENTO DE ERRO:
+               if(matPesquisa < 100 || matPesquisa > 999)
+               {
+                   printf("Matricula incorreta. Escolha novamente.\n");
+               }
+               else
+               {
+                  //PESQUISA SEQUENCIAL:
+                  //RESETAR:
+                  i = 0;
+                  output = -1; //NÃO ENCONTRADO
+                  while(i < indice)
+                  {
+                      if(matricula[i] == matPesquisa)
+                      {
+                          output = i; //VALOR ENCONTRADO
+                          break;
+                      }
+                      i++;
+                  }
+                  if(output == -1)
+                  {
+                      printf("Matricula: %i não localizada na pauta dos alunos.\n", matPesquisa);
+                  }
+                  else
+                  {
+                      printf("Dados: Aluno localizado %i sao:\n", output + 1);
+                      printf("Matricula: %i\n", matricula[output]);
+                      printf("Nota Bimestre 1: %.1f\n", bimestre[output]);
+                  }
+
+               }
+            }
+
         }
         else if(opcao == 4)
         {
-            printf("Relatorio 3\n");
+            if(indice == 0)
+            {
+                printf("Pauta Vazia.\n");
+            }
+            else
+            {
+                //BOLHA: Resetar
+                i = 0;
+                Ls = indice - 1;
+                do
+                {
+                    Flag = 0; //PAUTA ORDENADA
+                    for(i = 0; i < Ls; i++)
+                    {
+                        if(matricula[i] > matricula[i+1])
+                        {
+                            //MATRICULA:
+                            aux1 = matricula[i];
+                            matricula[i] = matricula[i+1];
+                            matricula[i+1] = aux1;
+                            Flag = 1;
+                            //NOTA:
+                            aux2 = bimestre[i];
+                            bimestre[i] = bimestre[i+1];
+                            bimestre[i+1] = aux2;
+                            Flag = 1;
+                        }
+                    }
+                    Ls--;
+                }while(Flag);
+                printf("Pauta em ordem de matricula:\n");
+                for(i = 0; i < indice; i++)
+                {
+                    printf("Dados do Aluno %i sao:\n", i + 1);
+                    printf("Matricula: %i\n", matricula[i]);
+                    printf("Nota Bimestre 1: %.1f\n\n", bimestre[i]);
+                }
+
+            }
         }
         else
         {
